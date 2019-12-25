88cd9fae4fc50d8fba61cb55eefff9f5bcdbd357
G_Polya <wlskaka4@gmail.com>
2019-12-25 10:01:27 +0900

    guild_server로 변경

--- maple_guild1.py
+++ maple_guild1.py
@@ -16,11 +16,11 @@ search_button.click()
 guilds = driver.find_elements_by_css_selector("div.col-12")
 
 # 이 부분 일반적이게 변경
-driver.implicitly_wait(5)
-finded_server = guilds[0].find_element_by_css_selector("div.search-result__item")
-guild_name = finded_server.find_element_by_css_selector("b.font-size-14").text
+driver.implicitly_wait(10)
+finded_server = guilds[0].find_element_by_css_selector("div.search-result__item")   # 서버 검색부분. Alpha길드는 크로아에도 있고 유니온에도 있음.
+guild_server = finded_server.find_element_by_css_selector("b.font-size-14").text
 
-if guild_name == server:
+if guild_server == server:    #
     guild_button = driver.find_element_by_css_selector("div.col-12")
     guild_button.click()
     driver.implicitly_wait(5)
